88d25863912e2014c7e2cc108d86a859469ff188
Adrian Martinez <aluism6@gmail.com>
2022-06-08 13:44:25 -0700

    Worked on images

--- server.js
+++ server.js
@@ -45,6 +45,7 @@ app.use(express.static('public'))
 app.use('/html',express.static(__dirname + 'public/html'))
 app.use('/css',express.static(__dirname + 'public/css'))
 app.use('/js',express.static(__dirname + 'public/js'))
+app.use('/png',express.static(__dirname + 'public/images/png'))
 
 app.get('/', function(req,res){
     res.sendFile(__dirname + '/public/index.html')
